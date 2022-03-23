e7d9fccf631450c53802930f18df2a62c09fe7b1
= <training-info@zenways.io>
2022-03-23 15:24:47 +0530

    Lazy loading route modules

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'lazy-routing-app';
+
+  user: any = {};
+
+  afterSubmit(){
+    alert(JSON.stringify(this.user));
+  }
+}
